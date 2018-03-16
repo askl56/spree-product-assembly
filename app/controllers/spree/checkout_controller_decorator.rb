483633e117a1c60d4efcee04aa58d8a6b762385c
Andrew Scott <amscott@openmailbox.org>
2018-03-16 00:13:18 +0000

    Update checkout_controller_decorator.rb

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -4,6 +4,9 @@ module Spree
       # Override because we don't want to remove unshippable items from the order
       # A bundle itself is an unshippable item
       def before_payment
+        if try_spree_current_user && try_spree_current_user.respond_to?(:payment_sources)
+          @payment_sources = try_spree_current_user.payment_sources
+        end
       end
     end
   end
